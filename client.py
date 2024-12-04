5cd2fdadf3c0c0cc647ff28fdd3f40de9b3c53a2
C-C-Coalback <argarfield18@gmail.com>
2024-12-04 15:39:45 +0100

    Client confirms message received

--- client.py
+++ client.py
@@ -3,3 +3,7 @@ import socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', 8089))
 client_socket.send(bytes("1#T#1#-2#5", 'UTF-8'))
+msg = client_socket.recv(1024)
+while msg:
+    print("Received:" + msg.decode())
+    msg = client_socket.recv(1024)
