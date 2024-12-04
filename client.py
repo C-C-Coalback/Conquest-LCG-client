492025f8d48f33c21ee90e6dcb73d8c9c572a51a
C-C-Coalback <argarfield18@gmail.com>
2024-12-04 15:25:32 +0100

    Very small change

--- client.py
+++ client.py
@@ -2,4 +2,4 @@ import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', 8089))
-client_socket.send(bytes("hello", 'UTF-8'))
+client_socket.send(bytes("1#T#1#-2#5", 'UTF-8'))
