fc55508ed314f7d0b01a5314aec7629ec7da8d5a
C-C-Coalback <argarfield18@gmail.com>
2024-12-12 10:54:38 +0100

    Can send info of what hand position was clicked on to server

--- PosFromClick.py
+++ PosFromClick.py
@@ -7,7 +7,7 @@ def pos_from_click(x, y):
         return "PASS"
     if check_for_action(x, y):
         return "ACTION"
-    if 319 < y < 376:
+    if 319 < y < 376: #PLANETS
         position = x - 60
         remainder = position % 165
         position = int(position / 165)
@@ -16,7 +16,7 @@ def pos_from_click(x, y):
         else:
             message = "Planet#" + str(position)
             return message
-    if 500 < y < 588:
+    if 500 < y < 588: #HQ PLAYER ONE
         position = x
         position = position - 300
         remainder = position % 80
@@ -25,7 +25,8 @@ def pos_from_click(x, y):
             return ""
         else:
             message = "P1#HQ#" + str(position)
-    if 125 < y < 213:
+            return message
+    if 125 < y < 213: #HQ PLAYER TWO
         position = x
         position = position - 300
         remainder = position % 80
@@ -34,5 +35,27 @@ def pos_from_click(x, y):
             return ""
         else:
             message = "P2#HQ#" + str(position)
+            return message
+    if 594 < y < 686:
+        position = x
+        position = position - 300
+        remainder = position % 80
+        position = int(position / 80)
+        print(position, remainder)
+        if 62 < remainder:
+            return ""
+        else:
+            message = "P1#Hand#" + str(position)
+            return message
+    if 24 < y < 116:
+        position = x
+        position = position - 200
+        remainder = position % 80
+        position = int(position / 80)
+        if 62 < remainder:
+            return ""
+        else:
+            message = "P2#Hand#" + str(position)
+            return message
 
     return message
