42b50919d74c4aa42abcb55486a7102d6c07b3bc
C-C-Coalback <argarfield18@gmail.com>
2024-12-12 10:42:55 +0100

    Can send info of what planet position was clicked on to server

--- PosFromClick.py
+++ PosFromClick.py
@@ -2,9 +2,19 @@ from PassCommand import check_for_pass
 from ActionChecker import check_for_action
 
 def pos_from_click(x, y):
+    message = ""
     if check_for_pass(x, y):
         return "PASS"
     if check_for_action(x, y):
         return "ACTION"
-    message = str(x) + "#" + str(y)
+    if 319 < y < 376:
+        position = x - 60
+        remainder = position % 165
+        position = int(position / 165)
+        if 84 < remainder:
+            pass
+        else:
+            message = "Planet#" + str(position)
+            return message
+
     return message
