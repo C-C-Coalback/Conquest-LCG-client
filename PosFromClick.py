a21396938790d8cf9528dc1b219171f31d283ba1
C-C-Coalback <argarfield18@gmail.com>
2024-12-12 10:50:22 +0100

    Can send info of what hq position was clicked on to server

--- PosFromClick.py
+++ PosFromClick.py
@@ -12,9 +12,27 @@ def pos_from_click(x, y):
         remainder = position % 165
         position = int(position / 165)
         if 84 < remainder:
-            pass
+            return message
         else:
             message = "Planet#" + str(position)
             return message
+    if 500 < y < 588:
+        position = x
+        position = position - 300
+        remainder = position % 80
+        position = int(position / 80)
+        if 62 < remainder:
+            return ""
+        else:
+            message = "P1#HQ#" + str(position)
+    if 125 < y < 213:
+        position = x
+        position = position - 300
+        remainder = position % 80
+        position = int(position / 80)
+        if 62 < remainder:
+            return ""
+        else:
+            message = "P2#HQ#" + str(position)
 
     return message
